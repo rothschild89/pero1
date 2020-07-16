bd4eb8ee17b7fcc02cff140ca2236c8400935f3c
rothschild89 <56435600+rothschild89@users.noreply.github.com>
2020-07-16 21:23:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-<script src="https://lmschool.ru/pl/lite/widget/script?id=230210"></script>
+document.addEventListener("DOMContentLoaded", function() {
+    var script = document.getElementById('46a5fbf4b31f3c647b68ee5297c311bc284ed2c2');
+
+	var par = script.parentNode;
+	script.parentNode.style.overflow = 'hidden';
+
+	var iframe = document.createElement('iframe');
+	iframe.src = 'https://lmschool.ru/pl/lite/widget/widget'
+		+ "?" + window.location.search.substring(1)
+		+ "&id=230210"
+		+ "&ref=" + encodeURIComponent(document.referrer)
+		+ "&loc=" + encodeURIComponent(document.location.href);
+	iframe.style.width = '100%';
+	iframe.style.height = '0px';
+	iframe.style.border = 'none';
+	iframe.style.overflow = 'hidden';
+    iframe.className = '162';
+	iframe.id = '60e90b432d13b6957941c7fa2b7f81ff3e9bbc10' + '_' + iframe.className;
+	// name можно получить изнутри iframe
+	iframe.name = iframe.className;
+
+	var iframeId = iframe.id;
+
+	var gcEmbedOnMessage = function(e) {
+		var insertedIframe = document.getElementById(iframeId);
+		if (!insertedIframe) {
+			return;
+		}
+
+		if (e.data.uniqName == '46a5fbf4b31f3c647b68ee5297c311bc284ed2c2') {
+			if (e.data.height) {
+			    if (e.data.iframeName) {
+					if (e.data.iframeName == iframe.name) {
+                        par.style.height = ( e.data.height ) + "px";
+						insertedIframe.style.height = (e.data.height) + "px";
+                    }
+                } else {
+                    par.style.height = ( e.data.height ) + "px";
+					insertedIframe.style.height = (e.data.height) + "px";
+                }
+            }
+		}
+	};
+
+	if (window.addEventListener) {
+		window.addEventListener("message", gcEmbedOnMessage, false);
+	}  else if (window.attachEvent) {
+		window.attachEvent('onmessage', gcEmbedOnMessage)
+	} else {
+		window['onmessage'] = gcEmbedOnMessage
+	}
+
+	script.parentNode.insertBefore(iframe, script);
+	par.removeChild( script )
+});
+
+var getLocation = function(href) {
+	var l = document.createElement("a");
+	l.href = href;
+	return l;
+};
+
+var currentScript = document.currentScript || (function() {
+	var scripts = document.getElementsByTagName('script');
+	return scripts[scripts.length - 1];
+})();
+
+var domain = ( (getLocation( currentScript.src )).hostname );

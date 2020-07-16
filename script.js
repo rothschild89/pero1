309581ed41c54bbc33fbd0d659a989575390dbdf
rothschild89 <56435600+rothschild89@users.noreply.github.com>
2020-07-16 21:22:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-<script id="46a5fbf4b31f3c647b68ee5297c311bc284ed2c2" src="https://lmschool.ru/pl/lite/widget/script?id=230210"></script>
+<script src="https://lmschool.ru/pl/lite/widget/script?id=230210"></script>
